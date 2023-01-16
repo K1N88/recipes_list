303c6a759a7fd58fb95f129de86f2fa46d0f8127
K1N88 <m-end-eleev@yandex.ru>
2023-01-16 14:28:25 +0300

    16 11 commit

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -25,11 +25,10 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 class SubscribeSerializer(AuthorSerializer):
     id = serializers.ReadOnlyField(source='author.id')
-    '''
     email = serializers.ReadOnlyField(source='author.email')
     username = serializers.ReadOnlyField(source='author.username')
     first_name = serializers.ReadOnlyField(source='author.first_name')
-    last_name = serializers.ReadOnlyField(source='author.last_name')'''
+    last_name = serializers.ReadOnlyField(source='author.last_name')
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
 

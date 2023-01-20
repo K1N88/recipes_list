2cdb1aa9c577e05d6d7c42e3a727b5f6d198f325
K1N88 <m-end-eleev@yandex.ru>
2023-01-20 12:47:32 +0300

    20 commit

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -59,7 +59,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeIngredientSerializer(serializers.ModelSerializer):
-    id = serializers.PrimaryKeyRelatedField(source='ingredient',
+    id = serializers.PrimaryKeyRelatedField(source='ingredient.id',
                                             read_only=True)
     name = serializers.ReadOnlyField(source='ingredient.name')
     measurement_unit = serializers.ReadOnlyField(
